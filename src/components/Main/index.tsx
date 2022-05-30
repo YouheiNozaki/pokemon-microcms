46d7e0827f66f01c31f329dcafd73f2c26f90c5e
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-05-30 22:56:30 +0900

    fix

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -27,14 +27,14 @@ export const Main = () => {
             onChange={(e) => onSelectGeneration(e.target.value)}
           >
             <option value={'ALL'}>ALL</option>
-            <option value={1}>第1世代</option>
-            <option value={2}>第2世代</option>
-            <option value={3}>第3世代</option>
-            <option value={4}>第4世代</option>
-            <option value={5}>第5世代</option>
-            <option value={6}>第6世代</option>
-            <option value={7}>第7世代</option>
-            <option value={8}>第8世代</option>
+            <option value={1}>1世代</option>
+            <option value={2}>2世代</option>
+            <option value={3}>3世代</option>
+            <option value={4}>4世代</option>
+            <option value={5}>5世代</option>
+            <option value={6}>6世代</option>
+            <option value={7}>7世代</option>
+            <option value={8}>8世代</option>
           </select>
           <input
             type="text"
